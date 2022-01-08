f3e8a03cf69db917f8586b9bbb0432d952c32c8a
David Braude <dabraude@gmail.com>
2022-01-08 19:27:24 +0000

    graceful exiting

--- pyliza/interface.py
+++ pyliza/interface.py
@@ -1,12 +1,18 @@
 import logging
 import typing
 
-from . import script
+from . import eliza
 
 
-def run_commandline(script_text: typing.Iterable[str]):
+def run_commandline(script: typing.Iterable[str]):
     log = logging.getLogger("pyliza")
     log.info("starting up Pyliza command line")
 
-    liza_script = script.ElizaScript(script_text)
-    print(liza_script.greet())
+    cmd_eliza = eliza.Eliza(script)
+    try:
+        user_response = input(cmd_eliza.greet())
+        while True:
+            user_response = input(cmd_eliza.respond_to(user_response))
+    except (KeyboardInterrupt, EOFError):
+        print("GOODBYE")
+        exit()
